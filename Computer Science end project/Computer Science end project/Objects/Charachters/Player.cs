b67a9d69603bdc12ae1bc88da6df9ce61bb09573
Zak Brown <96209266+ZakB659@users.noreply.github.com>
2022-06-30 15:27:14 +0100

    Update Player.cs

--- Computer Science end project/Computer Science end project/Objects/Charachters/Player.cs
+++ Computer Science end project/Computer Science end project/Objects/Charachters/Player.cs
@@ -8,7 +8,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Content;
 
-namespace Computer_Science_end_project
+namespace Project_end
 {
     class Player : Charachter
     {
@@ -35,7 +35,7 @@ namespace Computer_Science_end_project
             windowWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
             windowHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
             Direction = direction.None;
-            _Location = new Vector2(windowWidth/2, windowHeight/2);
+            _Location = new Vector2(windowWidth / 2, windowHeight / 2);
             _MovementSpeed = 2;
             Health = 10;
             Timetowait = 30;
@@ -50,90 +50,98 @@ namespace Computer_Science_end_project
             inputs = ks.GetPressedKeys();
             MouseState mouseState = Mouse.GetState();
 
-            if(movingroom == true)
+            if (movingroom == true)
             {
                 MoveroomAnimation();
             }
             else
-            {  
+            {
                 if (ks.IsKeyDown(Keys.W))
-                {                        
+                {
                     Movement.Y = _MovementSpeed * (Movement.Y - 1);
                     _Direction = direction.North;
-                    movinganimationX = 3;                        
+                    movinganimationX = 3;
                 }
                 if (ks.IsKeyDown(Keys.A))
                 {
-                    Movement.X = _MovementSpeed * (Movement.X - 1);                        
-                    _Direction = direction.West;                       
+                    Movement.X = _MovementSpeed * (Movement.X - 1);
+                    _Direction = direction.West;
                     movinganimationX = 1;
                 }
                 if (ks.IsKeyDown(Keys.D))
                 {
-                    Movement.X = _MovementSpeed * (Movement.X + 1);                        
-                    _Direction = direction.East;                        
+                    Movement.X = _MovementSpeed * (Movement.X + 1);
+                    _Direction = direction.East;
                     movinganimationX = 2;
                 }
                 if (ks.IsKeyDown(Keys.S))
                 {
-                     _Direction = direction.South;                       
-                     Movement.Y = _MovementSpeed * (Movement.Y + 1);
-                     movinganimationX = 0;
+                    _Direction = direction.South;
+                    Movement.Y = _MovementSpeed * (Movement.Y + 1);
+                    movinganimationX = 0;
                 }
-                if(ks.IsKeyDown(Keys.D & Keys.A)) { Movement.X = 0; movinganimationX = 0; _Direction = direction.None; }
-                if (ks.IsKeyDown(Keys.D & Keys.A)) { Movement.X = 0; movinganimationX = 0; _Direction = direction.None; }
-                if (ks.IsKeyDown(Keys.Space))
-                    {
-                        Timewaited++;
-
-                        if (Timewaited > Timetowait / shootingspeed)
-                        {
-
-                            projectiles.addprojectiles(content, Theplayer,_Movement);
-                            
-                            Timewaited = 0;
-                        }
-                    }
-
-
-                    _Location = _Location + Movement;
-                    
-
-                    if (Math.Abs(windowWidth/2 - _Location.X) < 150 && Math.Abs(0 - _Location.Y) < 30)
-                    {
-                        MoveroomAnimation();
-                    }
-
-                    if (Math.Abs(windowWidth/2 - _Location.X) < 100 && Math.Abs(windowHeight - _Location.Y) < 10)
-                    {
-                        
-                        MoveroomAnimation();
-                    }
-
-                    if (Math.Abs(0 - _Location.X) < 30 && Math.Abs(windowHeight/2 - _Location.Y) < 150)
-                    {
-                        MoveroomAnimation();
-                    }
-
-                    if (Math.Abs(windowWidth - _Location.X) < 30 && Math.Abs(windowHeight/2 - _Location.Y) < 150)
-                    {
-                        MoveroomAnimation();
-                    }
-
+               
                 
+                if (ks.IsKeyDown(Keys.D)&&ks.IsKeyDown(Keys.A)) { Movement.X = 0;  movinganimationX = 0; _Direction = direction.None; }
+                if (ks.IsKeyDown(Keys.W) && ks.IsKeyDown(Keys.S)) { Movement.Y = 0;  movinganimationX = 0; _Direction = direction.None; }
+
+                if (ks.IsKeyDown(Keys.Space))
+                {
+                    Timewaited++;
+
+                    if (Timewaited > Timetowait / shootingspeed)
+                    {
+                        bool stationary = false;
+                        if (Movement == new Vector2(0,0))
+                        {
+                            stationary = true;
+                        }
+
+                        projectiles.addprojectiles(content, Theplayer,_Movement,stationary);
+
+                        Timewaited = 0;
+                    }
+                }
+
+
+                _Location = _Location + Movement;
+
+
+                if (Math.Abs(windowWidth / 2 - _Location.X) < 150 && Math.Abs(0 - _Location.Y) < 30)
+                {
+                    MoveroomAnimation();
+                }
+
+                if (Math.Abs(windowWidth / 2 - _Location.X) < 100 && Math.Abs(windowHeight - _Location.Y) < 10)
+                {
+
+                    MoveroomAnimation();
+                }
+
+                if (Math.Abs(0 - _Location.X) < 30 && Math.Abs(windowHeight / 2 - _Location.Y) < 150)
+                {
+                    MoveroomAnimation();
+                }
+
+                if (Math.Abs(windowWidth - _Location.X) < 30 && Math.Abs(windowHeight / 2 - _Location.Y) < 150)
+                {
+                    MoveroomAnimation();
+                }
+
+
                 Movement.X = 0;
                 Movement.Y = 0;
-            }           
+            }
         }
 
         public void MoveroomAnimation()
         {
             switch (Direction)
             {
-                case direction.North:                   
+                case direction.North:
                     Movement.Y = _MovementSpeed * (Movement.Y - 1);
                     _Location = _Location + Movement;
-                    if (Math.Abs(windowWidth/2 - _Location.X) < 150 && Math.Abs(0 - _Location.Y) < 10)
+                    if (Math.Abs(windowWidth / 2 - _Location.X) < 150 && Math.Abs(0 - _Location.Y) < 10)
                     {
                         Roomchange.Y = windowHeight;
                         _Location = _Location + Roomchange;
@@ -141,54 +149,54 @@ namespace Computer_Science_end_project
                     movingroom = true;
                     break;
                 case direction.West:
-                    Movement.X = _MovementSpeed * (Movement.X - 1);                   
+                    Movement.X = _MovementSpeed * (Movement.X - 1);
                     _Location = _Location + Movement;
-                    if (Math.Abs(0 - _Location.X) < 10 && Math.Abs(windowHeight/2 - _Location.Y) < 100)
+                    if (Math.Abs(0 - _Location.X) < 10 && Math.Abs(windowHeight / 2 - _Location.Y) < 100)
                     {
-                        Roomchange.X= windowWidth;
+                        Roomchange.X = windowWidth;
                         _Location = _Location + Roomchange;
                     }
                     movingroom = true;
                     break;
-                case direction.South:                   
+                case direction.South:
                     Movement.Y = _MovementSpeed * (Movement.Y + 1);
                     _Location = _Location + Movement;
-                    if (Math.Abs(windowWidth/2 - _Location.X) < 150 && Math.Abs(windowHeight - _Location.Y) < 10)
+                    if (Math.Abs(windowWidth / 2 - _Location.X) < 150 && Math.Abs(windowHeight - _Location.Y) < 10)
                     {
                         Roomchange.Y = -windowHeight;
                         _Location = _Location + Roomchange;
                     }
                     movingroom = true;
                     break;
-                case direction.East:                    
+                case direction.East:
                     Movement.X = _MovementSpeed * (Movement.X + 1);
                     _Location = _Location + Movement;
-                    if (Math.Abs(windowWidth - _Location.X) < 10 && Math.Abs(windowHeight/2 - _Location.Y) < 100)
+                    if (Math.Abs(windowWidth - _Location.X) < 10 && Math.Abs(windowHeight / 2 - _Location.Y) < 100)
                     {
                         Roomchange.X = -windowWidth;
                         _Location = _Location + Roomchange;
                     }
                     movingroom = true;
-                    break;                   
+                    break;
             }
             Movement.X = 0;
             Movement.Y = 0;
 
-            if ((Math.Abs(windowWidth / 2 - _Location.X) < windowWidth/5 && Math.Abs(0 - _Location.Y) < windowHeight/10) || (Math.Abs(0 - _Location.X) < windowWidth / 10 && Math.Abs(windowHeight / 2 - _Location.Y) < windowHeight/7) || (Math.Abs(windowWidth / 2 - _Location.X) < windowWidth/6 && Math.Abs(windowHeight - _Location.Y) < windowHeight/10) || (Math.Abs(windowWidth - _Location.X) < windowWidth/6 && Math.Abs(windowHeight / 2 - _Location.Y) < windowHeight / 5))
+            if ((Math.Abs(windowWidth / 2 - _Location.X) < windowWidth / 5 && Math.Abs(0 - _Location.Y) < windowHeight / 10) || (Math.Abs(0 - _Location.X) < windowWidth / 10 && Math.Abs(windowHeight / 2 - _Location.Y) < windowHeight / 7) || (Math.Abs(windowWidth / 2 - _Location.X) < windowWidth / 6 && Math.Abs(windowHeight - _Location.Y) < windowHeight / 10) || (Math.Abs(windowWidth - _Location.X) < windowWidth / 6 && Math.Abs(windowHeight / 2 - _Location.Y) < windowHeight / 5))
             {
                 movingroom = false;
             }
         }
 
-        public override void loadcontent(ContentManager content,string name)
+        public override void loadcontent(ContentManager content, string name)
         {
             _Texture = content.Load<Texture2D>(@name);
         }
 
         public override void Draw(SpriteBatch spriteBatch)
-        {            
-            spriteBatch.Draw(_Texture, _Location,new Rectangle(32*movinganimationX,32*movinganimationY,32,32), Color.White,0,new Vector2(0,0),2,SpriteEffects.None,0);
-        }       
+        {
+            spriteBatch.Draw(_Texture, _Location, new Rectangle(32 * movinganimationX, 32 * movinganimationY, 32, 32), Color.White, 0, new Vector2(0, 0), 2, SpriteEffects.None, 0);
+        }
 
 
     }
